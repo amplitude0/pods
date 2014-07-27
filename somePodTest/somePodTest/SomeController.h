41d0f127450e2be35f6ec2fa859fe02ebcd14029
amplitude0 <eihwazz@Ernests-Mac-mini-4.local>
2014-07-27 17:54:46 +0200

    update

---
+++ somePodTest/somePodTest/SomeController.h
@@ -0,0 +1,13 @@
+//
+//  SomeController.h
+//  somePodTest
+//
+//  Created by amplitude0 on 27/07/14.
+//  Copyright (c) 2014 myName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SomeController : UIViewController
+
+@end
